5a38ac42ce3410d5509a6dcfa589e99e16526584
Tjom Emmanuel <tjomemmanuel@gmail.com>
2020-06-22 04:29:59 -0700

    Mise en place des abonnées sur la page d'acceuil home_screen: Following listView

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_social_ui/widgets/following_users.dart';
 
 class HomeScreen extends StatefulWidget {
 
@@ -8,7 +9,7 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen>
-with SingleTickerProviderStateMixin {
+    with SingleTickerProviderStateMixin {
 
 
   // Ajout du singleTickerProviderStateMixin et _tabController
@@ -23,40 +24,50 @@ with SingleTickerProviderStateMixin {
   }
 
 
-   @override
+  @override
   Widget build(BuildContext context) {
     // TODO: implement build
-   return new Scaffold(
-     appBar: AppBar(
-       backgroundColor: Colors.white,
-       brightness: Brightness.light,
-       iconTheme: IconThemeData(
-         color: Theme.of(context).primaryColor
-       ),
-       title: new Text(
-         'FRENZY',
-         style: TextStyle(
-             color: Theme.of(context).primaryColor,
-             fontSize: 30.0,
-             fontWeight: FontWeight.bold,
-             letterSpacing: 1.0
-         ),
-       ),
-       bottom: TabBar(
-         controller: _tabController,
-         labelColor: Theme.of(context).primaryColor,
-         labelStyle: new TextStyle(
-           fontSize: 18.0,
-           fontWeight: FontWeight.w600
-         ),
-         unselectedLabelStyle: new TextStyle(fontSize: 18.0),
-         indicatorWeight: 3.0,
-         tabs: <Widget>[
-           new Tab(text: 'Trending'),
-           new Tab(text: 'Lastest'),
-         ],
-       ),
-     ),
-   );
+    return new Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        brightness: Brightness.light,
+        iconTheme: IconThemeData(
+            color: Theme.of(context).primaryColor
+        ),
+        title: new Text(
+          'FRENZY',
+          style: TextStyle(
+              color: Theme.of(context).primaryColor,
+              fontSize: 30.0,
+              fontWeight: FontWeight.bold,
+              letterSpacing: 1.0
+          ),
+        ),
+        bottom: TabBar(
+          controller: _tabController,
+          labelColor: Theme.of(context).primaryColor,
+          labelStyle: new TextStyle(
+              fontSize: 18.0,
+              fontWeight: FontWeight.w600
+          ),
+          unselectedLabelStyle: new TextStyle(fontSize: 18.0),
+          indicatorWeight: 3.0,
+          tabs: <Widget>[
+            new Tab(text: 'Trending'),
+            new Tab(text: 'Lastest'),
+          ],
+        ),
+      ),
+      body: ListView(
+        children: <Widget>[
+          new Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              FollowingUsers(),
+            ],
+          )
+        ],
+      ),
+    );
   }
 }
